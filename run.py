178ae03901ff335b023ab3a49084b413cc8241dc
Ahmed Abdelrehim <88915626+aabdelre@users.noreply.github.com>
2023-01-07 20:45:38 +0200

    ModelTrained

---
+++ run.py
@@ -0,0 +1,10 @@
+import pygame, sys, cv2, numpy as np
+from pygame.locals import *
+from keras.models import load_model
+
+def main():
+    pygame.init()
+    
+
+if __name__ == "__main__":
+    main()
